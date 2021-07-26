ceb79737dee515d8809dd0e4c801daee9391ef71
Dushyant Ramful <dushyantramful@gmail.com>
2021-07-26 14:21:18 +0400

    1st rails exercise

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,15 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @answer = params['answer']
+    @coach_answer = if @answer.end_with?('?')
+                      'Silly question, get dressed and go to work!'
+                    elsif @answer == 'I am going to work'
+                      'Great!'
+                    else
+                      'I don\'t care, get dressed and go to work!'
+                    end
+  end
+end
